1f358ac43e6e030ab4adb2a14dcd497929f20195
marilia.lepore@gmail.com <marilia.lepore@gmail.com>
2020-09-27 01:22:11 -0300

    Conexão com BD

---
+++ ProjEmpresa/ProjEmpresa/Dao/EFContext.cs
@@ -0,0 +1,24 @@
+﻿using ProjEmpresa.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace ProjEmpresa.Dao
+{
+        public class EFContext : DbContext
+        {
+            public EFContext() : base("EFConnectionString") { }
+
+            protected override void OnModelCreating(DbModelBuilder modelBuilder)
+            {
+                modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+                base.OnModelCreating(modelBuilder);
+
+            }
+
+            public DbSet<Empresa> Empresas { get; set; }
+        }
+ }
---
+++ ProjEmpresa/ProjEmpresa/Models/Empresa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjEmpresa.Models
+{
+    public class Empresa
+    {
+        [Key]
+        public int idEmpresa { get; set; }
+        public string cnpj { get; set; }
+        public string nome { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime dataAbertura { get; set; }
+        public string atividade { get; set; }
+    }
+}
